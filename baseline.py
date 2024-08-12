5d6a4be23699e7231fd752e34d3bd0a04ff9ff10
amirali <amirali@DESKTOP-2P0IDPU.localdomain>
2024-08-12 16:50:37 -0400

    adding export def

--- baseline.py
+++ baseline.py
@@ -5,6 +5,7 @@ from sklearn.linear_model import LinearRegression
 import lightgbm as lgb
 import random
 import time
+import csv
 
 def mape(y_true, y_pred):
     return np.mean( np.abs(y_pred - y_true) / np.abs(y_true))
@@ -27,33 +28,39 @@ def lightgbm(X_train, y_train, X_test):
     gbm = lgb.train(params, train_data, num_boost_round=100)
     return gbm.predict(X_test, num_iteration=gbm.best_iteration)
 
+def export(df_y, results, time, dataset, algo):
+    with open(f'reg results/{algo}-{dataset.split('/')[-1]}', 'w', newline='') as f:    
+        write = csv.writer(f)
+        df_y.append('Time')
+        write.writerow([y for y in df_y])
+        for r in results:
+            write.writerow(r)
+        ll = (len(df_y)-1) * ['']
+        ll.append(time)
+        write.writerow(ll)
 
-# Load the dataset
-df = pd.read_csv('data/misc/Wine_quality.csv')
-df_y = [c for c in df.columns if (c[-1] == '-' or c[-1] == '+')]
+def calc_baseline(algo, dataset):
+    st = time.time()
+    results = []
+    for _ in range(20):
+        df = pd.read_csv(dataset)
+        df_y = [c for c in df.columns if (c[-1] == '-' or c[-1] == '+')]
+        X = df[[c for c in df.columns if (c[-1] != '-' and c[-1] != '+')]]
+        res = []
+        for target_column in df_y:
+            y = df[target_column]
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=int(100*random.random()))
 
-for i in df_y:
-    print(i,end=',\t')
-print('time')
+            if algo == 'linear':
+                y_pred = linear( X_train, y_train, X_test)
+            else:
+                y_pred = lightgbm( X_train, y_train, X_test)
 
-st = time.time()
-for _ in range(20):
-    df = pd.read_csv('data/misc/Wine_quality.csv')
-    df_y = [c for c in df.columns if (c[-1] == '-' or c[-1] == '+')]
-    X = df[[c for c in df.columns if (c[-1] != '-' and c[-1] != '+')]]
+            res.append(round(mape(y_test, y_pred),3))
+        results.append(res)
 
-    for target_column in df_y:
-        y = df[target_column]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=int(100*random.random()))
+    export(df_y, results , round(time.time()-st,2), dataset, algo)
 
-        #y_pred = linear( X_train, y_train, X_test)
-        y_pred = lightgbm( X_train, y_train, X_test)
 
-        # Evaluate the model with MAPE
-        print(f'{round(mape(y_test, y_pred),3)}', end='')
-        if target_column != df_y[-1]:
-            print(',\t', end='')
-
-    print()
-
-print( (len(df_y)) * ',',round(time.time()-st,2))
+calc_baseline('linear', 'data/misc/Wine_quality.csv')
+calc_baseline('lightgbm', 'data/misc/Wine_quality.csv')
